96f1247ac220b90f71f1dd2a458b31bb1d6959d1
BezaleelSainz <hueroacapulco@gmail.com>
2023-08-09 02:31:05 -0500

    add game credits canvas

--- Assets/Scripts/FinalBossScripts/CollisionFinalBoss.cs
+++ Assets/Scripts/FinalBossScripts/CollisionFinalBoss.cs
@@ -1,5 +1,160 @@
 
 
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class CollisionFinalBoss : MonoBehaviour
+//{
+//    AnimationStateChanger animationStateChanger;
+//    AudioSource audioSource;
+//    List<Transform> childObjects = new List<Transform>();
+
+//    MovingObject movingObject;
+//    CreateFinalBossLaser createFinalBossLaser;
+//    public CanvasGroup canvasGroup;
+
+
+
+//    public AudioClip explotion;
+
+//    public int points = 20;
+//    int collisionsCount = 0;
+//    int maxCollisions = 30;
+
+//    Color originalColor;
+//    Renderer r;
+//    bool changingColor = false;
+
+//    void Awake()
+//    {
+//        animationStateChanger = GetComponent<AnimationStateChanger>();
+//        foreach (Transform child in transform)
+//        {
+//            childObjects.Add(child);
+//        }
+
+//        r = GetComponent<Renderer>();
+//        originalColor = r.material.color;
+//    }
+
+//    void Start()
+//    {
+//        audioSource = GetComponent<AudioSource>();
+//        movingObject = GetComponent<MovingObject>();
+//        createFinalBossLaser = GetComponent<CreateFinalBossLaser>();
+
+//        // createFinalBossLaser1 = GetComponent<CreateFinalBossLaser>();
+
+
+//        //audioSource1 = GetComponent<AudioClip>();
+//    }
+
+//    void Update()
+//    {
+//        if (changingColor)
+//        {
+//            // Cambiar el color a rojo.
+//            r.material.color = Color.red;
+//            changingColor = false;
+
+//            // Iniciar la restauración del color después de medio segundo.
+//            StartCoroutine(RestoreColor());
+//        }
+//    }
+
+//    IEnumerator RestoreColor()
+//    {
+//        yield return new WaitForSeconds(0.5f);
+//        // Restaurar el color original.
+//        r.material.color = originalColor;
+//    }
+
+//    //coroutine
+//    public void ShowFinalMessage()
+//    {
+
+//        StartCoroutine(EsperarYMostrar());
+//        IEnumerator EsperarYMostrar()
+//        {
+//            Debug.Log("se esta llamano a la corutina");
+//            yield return new WaitForSeconds(3f); // Esperar 3 segundos
+//            canvasGroup.alpha = 1f; // Hace visible el texto y la imagen
+//            Debug.Log("Corutina terminada después de 3 segundos.");
+//        }
+
+
+//    }
+
+
+
+
+
+//    void OnCollisionEnter2D(Collision2D collision)
+//    {
+//        if (collision.gameObject.CompareTag("PlayerLaser"))
+//        {
+//            // Incrementar el contador de colisiones.
+//            collisionsCount++;
+
+
+//            // Cambiar el color a rojo.
+//            changingColor = true;
+
+//            audioSource.PlayOneShot(explotion);
+
+//            GameManager.score += points;
+
+//            // Comprobar si se alcanzó el límite de colisiones.
+//            if (collisionsCount == 10)
+//            {
+
+//                // Debug.Log("Cambiar la velocidad horizontal de bigboss");
+//                //BigbossMovement.GetComponent<MovingObject>().horizontalSpeed = 15;
+
+//                movingObject.horizontalSpeed = 6;
+//                createFinalBossLaser.velocidadLaser = 6;
+//                //createFinalBossLaser1.TiempoGeneracionDeLaser = 1;
+
+//                Debug.Log("se ha cambiado la velocidad horizontal de bigboss a 10");
+//                GameManager.score += 100;
+//            }
+//             if (collisionsCount == 20)
+//            {
+
+//                Debug.Log("se ha cambiado la velocidad horizontal de bigboss a 20");
+//                //BigbossMovement.GetComponent<MovingObject>().horizontalSpeed = 15;
+
+//                movingObject.horizontalSpeed = 9;
+//                createFinalBossLaser.velocidadLaser = 9;
+//                // createFinalBossLaser1.TiempoGeneracionDeLaser = .5f;
+
+
+//                ShowFinalMessage();
+//                GameManager.score += 500;
+//            }
+//            // Comprobar si se alcanzó el límite de colisiones.
+//            if(collisionsCount >= maxCollisions)
+//            {
+
+
+//                audioSource.Play();
+//                animationStateChanger.ChangeAnimationState("Destroy", 1.3f);
+
+//                //ShowFinalMessage();
+//                Destroy(gameObject, 1.4f);
+
+//                GameManager.score += 1000;
+
+
+
+//            }
+
+//        }
+//    }
+//}
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,9 +173,11 @@ public class CollisionFinalBoss : MonoBehaviour
 
     public AudioClip explotion;
 
+    public bool estaDestruido = false;
+
     public int points = 20;
     int collisionsCount = 0;
-    int maxCollisions = 5;
+    int maxCollisions = 3;
 
     Color originalColor;
     Renderer r;
@@ -43,7 +200,8 @@ public class CollisionFinalBoss : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
         movingObject = GetComponent<MovingObject>();
         createFinalBossLaser = GetComponent<CreateFinalBossLaser>();
-       // createFinalBossLaser1 = GetComponent<CreateFinalBossLaser>();
+
+        // createFinalBossLaser1 = GetComponent<CreateFinalBossLaser>();
 
 
         //audioSource1 = GetComponent<AudioClip>();
@@ -69,10 +227,12 @@ public class CollisionFinalBoss : MonoBehaviour
         r.material.color = originalColor;
     }
 
-    public void MakeVisible()
-    {
-        canvasGroup.alpha = 1f; // Hace visible el texto y la imagen
-    }
+    //coroutine
+
+
+
+
+
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -80,7 +240,7 @@ public class CollisionFinalBoss : MonoBehaviour
         {
             // Incrementar el contador de colisiones.
             collisionsCount++;
-          
+
 
             // Cambiar el color a rojo.
             changingColor = true;
@@ -93,7 +253,7 @@ public class CollisionFinalBoss : MonoBehaviour
             if (collisionsCount == 10)
             {
 
-               // Debug.Log("Cambiar la velocidad horizontal de bigboss");
+                // Debug.Log("Cambiar la velocidad horizontal de bigboss");
                 //BigbossMovement.GetComponent<MovingObject>().horizontalSpeed = 15;
 
                 movingObject.horizontalSpeed = 6;
@@ -103,7 +263,7 @@ public class CollisionFinalBoss : MonoBehaviour
                 Debug.Log("se ha cambiado la velocidad horizontal de bigboss a 10");
                 GameManager.score += 100;
             }
-             if (collisionsCount == 20)
+            if (collisionsCount == 20)
             {
 
                 Debug.Log("se ha cambiado la velocidad horizontal de bigboss a 20");
@@ -111,22 +271,30 @@ public class CollisionFinalBoss : MonoBehaviour
 
                 movingObject.horizontalSpeed = 9;
                 createFinalBossLaser.velocidadLaser = 9;
-               // createFinalBossLaser1.TiempoGeneracionDeLaser = .5f;
+                // createFinalBossLaser1.TiempoGeneracionDeLaser = .5f;
 
 
-
+                
                 GameManager.score += 500;
             }
             // Comprobar si se alcanzó el límite de colisiones.
-            if(collisionsCount >= maxCollisions)
+            if (collisionsCount >= maxCollisions)
             {
+
+
                 audioSource.Play();
                 animationStateChanger.ChangeAnimationState("Destroy", 1.3f);
-                // Si se alcanzó el límite, destruir el objeto principal.
+
+                //ShowFinalMessage();
+                estaDestruido = true;
                 Destroy(gameObject, 1.4f);
+
                 GameManager.score += 1000;
-                MakeVisible();
+
+
+
             }
+
         }
     }
 }
---
+++ Assets/VerificarDestruccion.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using UnityEngine;
+
+public class VerificarDestruccion : MonoBehaviour
+{
+    public CollisionFinalBoss collisionFinalBoss;
+    public CanvasGroup canvasGroup;
+
+    private void Update()
+    {
+        if (collisionFinalBoss != null && collisionFinalBoss.estaDestruido)
+        {
+
+
+            ShowFinalMessage();
+            Debug.Log("El objeto ha sido destruido.");
+        }
+    }
+    public void ShowFinalMessage()
+    {
+
+        StartCoroutine(EsperarYMostrar());
+        IEnumerator EsperarYMostrar()
+        {
+            Debug.Log("se esta llamano a la corutina");
+            yield return new WaitForSeconds(3f); // Esperar 3 segundos
+            canvasGroup.alpha = 1f; // Hace visible el texto y la imagen
+            Debug.Log("Corutina terminada después de 3 segundos.");
+        }
+
+
+    }
+}
